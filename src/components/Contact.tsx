a6297c9c3d701f7f40432c561ce6f9bd73b6f50e
Jatin Vashishtha <sjatin114@gmail.com>
2026-02-12 11:14:35 +0530

    Fix Netlify contact form v2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -45,12 +45,8 @@ const Contact = () => {
 
                     <div className="bg-white dark:bg-slate-950 p-8 md:p-10 rounded-[2rem] border border-slate-100 dark:border-slate-800 shadow-2xl shadow-slate-200/40 dark:shadow-none transition-colors">
                         <form
-                            method="POST"
-                            data-netlify="true"
-                            data-netlify-honeypot="bot-field"
                             name="contact"
-                            action="/"
-                            className="space-y-5">
+                            className="space-y-5" netlify>
                             <input type="hidden" name="form-name" value="contact" />
                             <input type="hidden" name="bot-field" />
 
